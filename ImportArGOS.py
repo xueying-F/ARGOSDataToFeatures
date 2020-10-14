d0c2ba0ea2febb86c72eff1234c46fc9ce06522a
xueying-F <xueying.feng@duke.edu>
2020-10-14 14:38:38 -0400

    Add spatial reference

--- ImportArGOS.py
+++ ImportArGOS.py
@@ -13,9 +13,18 @@
 # Import modules
 import sys, os, arcpy
 
+#allow arcpy to overwrite outputs
+arcpy.env.overwriteOutput = True
+
 # Set input variables (Hard-wired)
 inputFile = 'V:\\ARGOSTracking\\Data\\ARGOSData\\1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+outputSR = arcpy.SpatialReference(54002)
+
+# Create an empty feature class to which we'll add feature
+outPath, outName = os.path.split(outputFC)
+arcpy.CreateFeatureclass_management(outPath, outName,"POINT",'','','',outputSR)
 
 #%% Construct a while loop to iterate through all lines in the datafile
 # Open the ARGOS data file for reading
